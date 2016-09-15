924b0cfba64967a759eb89f663bec36dd4ba1f56
David Martinez <davidmgilo@gmail.com>
2016-09-15 21:11:56 +0200

    enganxar a una variable

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-//Un comentari Cntl + /
-
 $name = "David";
 
-echo 'Hola $name!'; // Entre cometes simples, no detecta que sigui una variable sino que ho agafa literal
+echo "Hola ${name}a";
