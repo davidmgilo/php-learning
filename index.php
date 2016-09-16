af4633425fd36b707dd7216b58f4302399b5ddbd
David Martinez <davidmgilo@gmail.com>
2016-09-16 16:15:34 +0200

    adeu comentaris

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
-//$greeting = "Hola " . $_GET['name'] ."!";
-
-//ARRAYS o vectors
-// ESTRUCTURA DE DADES;
-// Arrays PHP totes les estructures: Maps, Piles, Vectors, Llistes, etc.
-
-//$names = array();
-
 $names = [
     'Sergi',
     'Xavi',
