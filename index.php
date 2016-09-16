a7b5f862d50b32c42365ca1b24e27ae96f9de380
David Martinez <davidmgilo@gmail.com>
2016-09-16 17:04:52 +0200

    phpinfo

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
 <?php
 
-//ARRAYS ASSOCIATIUS
-
-$names = [ //PER DEFECTE
-    0 => 'Sergi',
-    1 => 'Xavi',
-    2 => 'Romeu'
-];
-
-$person = [
-    'name' => 'Sergi',
-    'surname' => 'Tur',
-    'surname2' => 'Badenas',
-    'edat' => 38,
-    'married' => true
-];
-
-$married = true;
-$married = false;
-
-echo $names[0];
-echo $person['surname'];
+phpinfo();
 
 require 'template.php';
