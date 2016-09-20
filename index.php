9dbc6ae97fd9195424066c84557e50dc4146cfb4
David Martinez <davidmgilo@gmail.com>
2016-09-20 17:59:43 +0200

    popo

--- index.php
+++ index.php
@@ -6,19 +6,25 @@ class Task {
 
     public $description;
 
-    public function __construct($description)
+    public $completed;
+
+    public function __construct($description,$completed)
     {
         $this->description = $description;
+        $this->completed = $completed;
     }
 }
 
-$task = new Task("Aprendre PHP");
-$task2 = new Task("Netejar habitació");
-$task3 = new Task("Comprar pa");
-$task4 = new Task("Sortir més");
+//POJO
+//POPO
+// PLAIN OLD OBJECT
+$task = new Task("Aprendre PHP", false);
+$task2 = new Task("Netejar habitació", true);
+$task3 = new Task("Comprar pa", false);
+$task1 = new Task("Sortir més");
 //$task2 = new Task();
 //$task3 = new Task();
 
-//var_dump($task)
+var_dump($task);
 
 require 'template.php';
