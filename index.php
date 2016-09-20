7b2b1a8b17f5b5f86af6754075a71fc58b849140
David Martinez <davidmgilo@gmail.com>
2016-09-20 17:48:25 +0200

    constructor

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
 require 'functions.php';
 
 class Task {
-    public function __construct()
+    public function __construct($a,$b,$c)
     {
-        echo "nova tasca construida";
+//        echo "nova tasca construida";
+        echo $a;
+        echo $b;
+        echo $c;
     }
 }
 
-$task = new Task();
-$task2 = new Task();
-$task3 = new Task();
+$task = new Task('patata','b','c');
+//$task2 = new Task();
+//$task3 = new Task();
 
 //var_dump($task)
 
