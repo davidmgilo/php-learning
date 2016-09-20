6f216a7c89f60bf3d093b24a0bd743ba2423e882
David Martinez <davidmgilo@gmail.com>
2016-09-20 17:46:31 +0200

    constructor

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 require 'functions.php';
 
 class Task {
-
+    public function __construct()
+    {
+        echo "nova tasca construida";
+    }
 }
 
 $task = new Task();
+$task2 = new Task();
+$task3 = new Task();
+
+//var_dump($task)
 
 require 'template.php';
