da085c1c9bc2e3ec89af9b397d23e827eb47d904
David Martinez <davidmgilo@gmail.com>
2016-09-20 18:04:25 +0200

    class

--- index.php
+++ index.php
@@ -2,30 +2,23 @@
 
 require 'functions.php';
 
-class Task {
+require 'Task.php';
 
-    public $description;
-
-    public $completed;
-
-    public function __construct($description,$completed)
-    {
-        $this->description = $description;
-        $this->completed = $completed;
-    }
-}
 
 //POJO
 //POPO
 // PLAIN OLD OBJECT
 // CONTENEDOR / ESTRUCTURA DE DADES
 $task = new Task("Aprendre PHP", false);
-$task2 = new Task("Netejar habitació", true);
-$task3 = new Task("Comprar pa", false);
-$task1 = new Task("Sortir més");
+var_dump($task->description);
+var_dump($task->completed);
+
+//$task2 = new Task("Netejar habitació", true);
+//$task3 = new Task("Comprar pa", false);
+//$task1 = new Task("Sortir més");
 //$task2 = new Task();
 //$task3 = new Task();
 
-var_dump($task);
+//var_dump($task);
 
 require 'template.php';
