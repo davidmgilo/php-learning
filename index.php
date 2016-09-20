68957006980083ef52f9fd69745275cfb1e707c1
David Martinez <davidmgilo@gmail.com>
2016-09-20 19:41:40 +0200

    connexio + errors

--- index.php
+++ index.php
@@ -4,7 +4,19 @@ require 'functions.php';
 
 require 'Task.php';
 
+// PDO: PHP Data Objects library/biblioteca
+// Permet agafar dades de la bbdd i mapejar-les.
 
 
 
-require 'template.php';
+try{
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=prova','root','');
+}
+catch (PDOException $e){
+    die("Ha hagut un error durant la connexió. Missatge: " . $e->getMessage());
+}
+
+
+
+
+//require 'template.php';
