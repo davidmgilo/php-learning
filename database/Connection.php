22f399efbec543ec677a2bf531e95e4f9be4be7d
David Martinez <davidmgilo@gmail.com>
2016-09-21 19:43:45 +0200

    configuracio 2 dependency injection

--- database/Connection.php
+++ database/Connection.php
@@ -5,19 +5,12 @@
 
 class Connection
 {
-    public static function make(){
+    //dependency injection : depen de la configuració , cal passar per paràmetres
+    public static function make($config){
         try {
-            $config = [
-                'dbtype' => 'mysql',
-                'dbname' => 'prova',
-                'username' => 'root',
-                'password' => '',
-                'dbhost' => '127.0.0.1'
-            ];
 
 
-            var_dump($config['dbtype'] . ':host=' .
-                $config['dbhost'] . ';dbname='. $config['dbname']);
+
             //$pdo = new PDO('mysql:host=127.0.0.1;dbname=prova', 'root', '');
             $pdo = new PDO($config['dbtype'] . ':host=' .
                 $config['dbhost'] . ';dbname='. $config['dbname'],
