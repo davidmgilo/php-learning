9a98961b51040409db2d563f8edceee04be0055f
David Martinez <davidmgilo@gmail.com>
2016-09-22 20:30:27 +0200

    DRY WET

--- index.php
+++ index.php
@@ -18,3 +18,6 @@ $query = new QueryBuilder($pdo);
 $tasks = $query->all("todos");
 
 require 'index.template.php';
+
+//DRY: Don't repeat yourself -- El que s'ha de fer
+//WET: WRITE EVERYTHING TWICE -- El que no s'ha de fer
