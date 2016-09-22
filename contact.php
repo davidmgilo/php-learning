d63cc75fa9dfd2f91584378cd7d8e6905c8ac7c0
David Martinez <davidmgilo@gmail.com>
2016-09-22 21:12:19 +0200

    menu

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam asperiores assumenda commodi corporis debitis delectus eius eos expedita facere fugit maxime non, quasi saepe sapiente similique tempore temporibus! Sunt.
+</body>
+</html>
