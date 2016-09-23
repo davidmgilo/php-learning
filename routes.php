cfc68f37ee59b9b9d35b5541f3956890061a96ac
David Martinez <davidmgilo@gmail.com>
2016-09-23 16:40:47 +0200

    a fer video 17 + form 1

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 return [
     '' => 'controllers/tasks.php',
     'persons' => 'controllers/persons.php',
-    ''
+    'add/person' => 'controllers/addperson.php',
     'about' => 'controllers/about.php',
     'contact' => 'controllers/contact.php',
 ];
