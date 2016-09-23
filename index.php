1385230f42b251b8221acd623f9427d81f147fc8
David Martinez <davidmgilo@gmail.com>
2016-09-23 15:50:45 +0200

    routes cool 3

--- index.php
+++ index.php
@@ -5,14 +5,18 @@ require "core/bootstrap.php";
 
 require "routes.php";
 
-$uri = trim($_SERVER['REQUEST_URI'],'/');
+//Programació orientada a objectes - evitar funció, millor mètodes
+
+//HTTP -> Requests -> Responseç
+//
+//$request = new Request();
+//$uri = $request->uri();
+
+$uri = Request::uri();
+
 
 if(array_key_exists($uri,$routes)){
     require $routes[$uri];
 } else{
     throw new Exception("No s'ha trobat la ruta");
 }
-
-
-//DRY: Don't repeat yourself -- El que s'ha de fer
-//WET: WRITE EVERYTHING TWICE -- El que no s'ha de fer
